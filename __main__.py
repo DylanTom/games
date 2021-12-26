2bd98794b94763084de17f361b19b2d42b74c551
Dylan Tom <tomd@bxscience.edu>
2021-12-26 18:58:05 -0500

    finished sudoku

--- __main__.py
+++ __main__.py
@@ -1,8 +1,20 @@
 """
-THe primary application script
+The primary application script
 """
 
 from sudoku import *
 
+def sudoku():
+	board = []
+	for i in range(Sudoku.SIZE):
+		row = []
+		for j in range(Sudoku.SIZE):
+			row.append(int(input(f'What is in ({i+1},{j+1})? ')))
+		board.append(row)
+	Sudoku(board).run()
+
 if __name__ == '__main__':
-	Sudoku(Sudoku.EASY).run()
+	print('What game do you want to play?')
+	game = input('Enter \'s\' for sudoku ')
+	if game == 's':
+		sudoku()
--- sudoku.py
+++ sudoku.py
@@ -2,6 +2,33 @@
 A class for sudoku.
 """
 
+import time
+
+# Test boards
+EASY = [
+	[0,0,0,0,0,4,9,0,0],
+	[5,4,0,7,0,6,0,0,0],
+	[8,0,3,1,0,0,0,7,0],
+	[2,6,8,0,1,7,0,5,4],
+	[0,0,0,0,5,8,6,0,0],
+	[4,1,0,0,3,2,8,9,7],
+	[0,0,0,0,0,1,0,4,3],
+	[6,0,4,0,0,0,1,0,9],
+	[0,3,0,8,0,0,0,0,0]
+]
+
+HARD = [
+	[8,0,0,0,0,0,0,0,0],
+	[0,0,3,6,0,0,0,0,0],
+	[0,7,0,0,9,0,2,0,0],
+	[0,5,0,0,0,7,0,0,0],
+	[0,0,0,0,4,5,7,0,0],
+	[0,0,0,1,0,0,0,3,0],
+	[0,0,1,0,0,0,0,6,8],
+	[0,0,8,5,0,0,0,1,0],
+	[0,9,0,0,0,0,4,0,0]
+]
+
 class Sudoku:
 	"""
 	
@@ -10,51 +37,33 @@ class Sudoku:
 	SIZE = 9
 	BOX_SIZE = 3
 
-	EASY = [
-		[0,0,0,0,0,4,9,0,0],
-		[5,4,0,7,0,6,0,0,0],
-		[8,0,3,1,0,0,0,7,0],
-		[2,6,8,0,1,7,0,5,4],
-		[0,0,0,0,5,8,6,0,0],
-		[4,1,0,0,3,2,8,9,7],
-		[0,0,0,0,0,1,0,4,3],
-		[6,0,4,0,0,0,1,0,9],
-		[0,3,0,8,0,0,0,0,0]
-	]
-
-	HARD = [
-		[8,0,0,0,0,0,0,0,0],
-		[0,0,3,6,0,0,0,0,0],
-		[0,7,0,0,9,0,2,0,0],
-		[0,5,0,0,0,7,0,0,0],
-		[0,0,0,0,4,5,7,0,0],
-		[0,0,0,1,0,0,0,3,0],
-		[0,0,1,0,0,0,0,6,8],
-		[0,0,8,5,0,0,0,1,0],
-		[0,9,0,0,0,0,4,0,0]
-	]
-
 	def __init__(self, board):
 		"""
-		Initializes the sudoku board
+		Initializes the sudoku board.
 
-		Parameter board: the sudoku grid to solve
+		Parameter board: the sudoku grid to solve, must be valid
 		Precondition: board is a 2D table of size, Sudoku.SIZE
 		"""
 		self.board = board
 
+
 	def run(self):
 		"""
-		Main script to run the sudoku solver
+		Runs the sudoku solver.
 		"""
 		print("Input board is...")
 		self._display_board()
+		start = time.perf_counter()
 		self._solve()
+		print(f'Solved in {time.perf_counter() - start} seconds')
 		print("Solved board is...")
-		self.display_board()
+		self._display_board()
+
 
 	def _display_board(self):
-		print()
+		"""
+		Displays the board in an easy to read format.
+		"""
 		for row in range(Sudoku.SIZE):
 			line = ""
 			if row % Sudoku.BOX_SIZE == 0 and row != 0:
@@ -66,22 +75,29 @@ class Sudoku:
 			print(line)
 		print()
 
+
 	def _solve(self):
 		"""
-		The main function to solve the sudoku board
+		Returns True if the board is solved or False if the board is not.
+
+		The main function to solve the sudoku board. 
+		- Finds the next empty square.
+		- Makes a guess from 1 to 9, inclusive. 
+		- Verifies that the guess is valid.  
+		- Fills the appropriate square with that number.
+		- Recursive call to solve the rest of the puzzle.
+		- Backtracks if the recursive call fails.
 		"""
 		r, c = self._next_empty_square()
 
-		if r == -1:
+		if r is None:
 			return True
 		
 		for guess in range(1,10):
 			if self._is_valid(guess,r,c):
-				self.board[r][c] == guess
-				# Recursive call to solve the rest of the puzzle
+				self.board[r][c] = guess
 				if self._solve():
 					return True
-			# Backtrack
 			self.board[r][c] = 0
 		
 		return False	
@@ -89,31 +105,44 @@ class Sudoku:
 
 	def _next_empty_square(self):
 		"""
-		Returns a pair r, c of a square that is empty
+		Returns a pair r, c of a square that is empty or None if there are
+		no more empty squares.
 
-		Helper method to solve()
+		Helper method to solve().
 		"""
 		for r in range(Sudoku.SIZE):
 			for c in range(Sudoku.SIZE):
 				if self.board[r][c] == 0:
 					return r, c
 		
-		return -1, -1
+		return None, None
 
 	def _is_valid(self, guess, row, col):
 		"""
 		Returns True if guess is a legal guess
+
+		Helper method to solve(). Verifies that the guess does not appear in 
+		the row, column, or its own 3 x 3 grid. 
+
+		Parameter guess: A random guess to fill the square at row, col
+		Precondition: guess is an int between 1 and 9, inclusively
+
+		Parameter row: The row of the empty square
+		Precondition: row is an int between 1 and 9, inclusively
+
+		Parameter col: The column of the empty square
+		Precondition: col is an int between 1 and 9, inclusively
 		"""
-		# Verifies the row is valid
+		# Verifies that the row is valid
 		if guess in self.board[row]:
 			return False
 
-		# Verifies the column is valid
+		# Verifies that the column is valid
 		col_tracker = [self.board[r][col] for r in range(Sudoku.SIZE)]
 		if guess in col_tracker:
 			return False
 		
-		# Verifies each 3 x 3 grid is valid
+		# Verifies that the 3 x 3 grid is valid
 		row_begin = (row // Sudoku.BOX_SIZE) * Sudoku.BOX_SIZE
 		col_begin = (col // Sudoku.BOX_SIZE) * Sudoku.BOX_SIZE
 
